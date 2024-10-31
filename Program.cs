5f5d3525c925654a8229db923a6070d8d9faf972
nhatpv <nhatpv@vietmap.vn>
2024-10-31 17:14:35 +0700

    feat: CRU - with db context

--- Program.cs
+++ Program.cs
@@ -1,46 +1,54 @@
-using CourseAPI.Dto;
-using CourseAPI.Dtos;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+// Config Swagger middleware
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.DocumentName = "TodoAPI";
+    config.Title = "TodoAPI v1";
+    config.Version = "v1";
+});
+
 var app = builder.Build();
+// Only show swagger in develop mode
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi(config =>
+    {
+        config.DocumentTitle = "TodoAPI";
+        config.Path = "/swagger";
+        config.DocumentPath = "/swagger/{documentName}/swagger.json";
+        config.DocExpansion = "list";
+    });
+}
 
-List<GetCourseDto> courses = [
-    new (1, "Note1"),
-    new (2, "Note2"),
-    new (3, "Note3")
+app.MapGet("/todoitems", async (TodoDb db) => await db.Todos.ToListAsync());
 
-];
+app.MapGet("/todoitems/complete", async (TodoDb db) => await db.Todos.Where(t => t.IsComplete).ToListAsync());
 
-app.MapGet("courses", () => courses).WithName("getCourses");
+app.MapGet("/todoitems/{id}", async (int id, TodoDb db) => await db.Todos.FindAsync(id) is Todo todo ? Results.Ok(todo) : Results.NotFound());
 
-app.MapGet("course/{id}",(int id) => {
-    return courses.Find(course => course.Id == id);
-}).WithName("getCourseWithId");
+app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
+{
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
 
-app.MapPost("courses", (CreateCourseDto newCourse) => {
-    int id = courses.Count + 1;
-    GetCourseDto course = new (id, newCourse.Des);
-    courses.Add(course);
-    return Results.CreatedAtRoute("getCourseWithId", new {id = id}, course);
+    return Results.Created($"/todoitems/{todo.Id}", todo);
 });
 
-app.MapPut("course/{id}", (int id, CreateCourseDto updatedCourse) => {
-    GetCourseDto? currCourse = courses.Find(course=> course.Id == id);
-    if (currCourse == null) {
-        return Results.NotFound();
-    }
-    GetCourseDto newCourse = new GetCourseDto(id, updatedCourse.Des);
-    courses[id - 1] = newCourse;
-    return Results.Ok();
-});
-
-app.MapDelete("course/{id}", (int id) => {
-    int currId = courses.FindIndex(course => course.Id == id);
-    if (currId == -1) {
-        return Results.NoContent();
-    }
-    courses.RemoveAt(currId);
-    return Results.Ok();
+app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo,TodoDb db) => 
+{
+    var todo = await db.Todos.FindAsync(id);
+    if (todo is null) return Results.NotFound();
+    todo.Name = inputTodo.Name;
+    todo.IsComplete = inputTodo.IsComplete;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
 });
 
 app.Run();
